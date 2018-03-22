c5f74c06af95848c749c5de825f737ea2ff4733b
Mike C <mikecronin92@gmail.com>
2018-03-22 21:35:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,13 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def bat
+def batch_badge_creator(clients)
+  clients_badge = []
+  clients.each {|client|
+    clients_badge << badge_maker(client)
+  }
+  return clients_badge
+end
+
+def assign_room(clients)
+  clients.each_with_in
